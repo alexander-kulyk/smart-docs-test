4250fa3fe06ab59290e6ea463ece094c03ed85b0
alexander-kulyk <kulyk.alexander@gmail.com>
2023-04-03 14:55:20 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://alexander-kulyk.github.io/smart-docs-test/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/smart-docs-test',
+  baseUrl: 'https://alexander-kulyk.github.io/smart-docs-test',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
